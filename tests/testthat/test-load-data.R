686ac2037b0de6e7cb6f01c86e68291c36e89c9c
Cameron Roach <camroach87@gmail.com>
2021-02-17 18:00:03 +1100

    Add load data tests

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,7 +1,13 @@
-test_that("Load data", {
-  
+library(dplyr)
+
+test_that("load data", {
+  setwd("../..")
+  pod_data <- load_data("inst/extdata/pod_ds_task1/")
   # Test hourly weather values (in interpolated df) still match with raw data.
   # Test interpolated values are between raw hourly values.
-  
   # Test max and min dates correct for each data drop. Probably need to create a table for this.
+  
+  expect_equal(pod_data, arrange(pod_data, datetime))   # test datetime order
+  expect_true(all(!is.na(pod_data)))                    # test NA values
+  
 })
