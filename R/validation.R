b7fa5e235c4681a797a2e56191201c82c402e10e
Cameron Roach <camroach87@gmail.com>
2021-02-22 14:51:34 +1100

    Add jump argument to cross validation. Closes #5.

--- R/validation.R
+++ R/validation.R
@@ -4,6 +4,7 @@
 #' @param start_date (date) Date to start creating folds from.
 #' @param horizon (int) Number of days making up each out-of-sample test set.
 #' @param iterations (int) Number of training and test sets to create.
+#' @param jump (int) Number of days to move forward each iteration. Defaults to the horizon length.
 #'
 #' @return List with train and test elements for each iteration.
 #' @export
@@ -14,7 +15,7 @@
 #' library(lubridate)
 #' dts <- ymd("2020-01-01") + minutes(30)*0:(48*14-1)
 #' cv_folds <- cv_ts_folds(dts, ymd("2020-01-07"), 2, 3)
-cv_ts_folds <- function(x, start_date, horizon, iterations) {
+cv_ts_folds <- function(x, start_date, horizon, iterations, jump = horizon) {
   if (any(sort(x) != x)) stop("Datetime values must be ordered.")
   
   list_cv <- list()
@@ -23,7 +24,9 @@ cv_ts_folds <- function(x, start_date, horizon, iterations) {
     list_cv[[i]][["train"]] <- which(x < start_date)
     list_cv[[i]][["test"]] <- which(x >= start_date & 
                                       x < start_date + days(horizon))
-    start_date <- start_date + days(1)
+    # TODO: throw warning if test set is empty, i.e., start_date + jump is
+    # beyond data range.
+    start_date <- start_date + days(jump)
   }
   
   list_cv
