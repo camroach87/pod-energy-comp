c5e74e1c283e91cd28ba90f4e73fc58a72cf572c
Cameron Roach <camroach87@gmail.com>
2021-02-23 11:57:45 +1100

    Add data loading tests

---
+++ tests/testthat/test-load-data.R
@@ -0,0 +1,29 @@
+setwd("../..")
+demand.data <- load_demand_data("inst/extdata/pod_ds_task1")
+
+test_that("public holidays load", {
+  # Test holidays
+  pub_hol_vec <- ymd(c("2017-8-28", "2017-12-25", "2017-12-26", "2018-1-1", 
+                       "2018-4-2"))
+  pub_hol_vec <- demand.data %>% 
+    filter(date(datetime) %in% pub_hol_vec) %>% 
+    pull(public_holiday)
+  expect_true(all(pub_hol_vec == 1))
+  # Test non-holidays
+  pub_hol_vec <- ymd(c("2017-8-27", "2017-12-12", "2017-12-24", "2018-1-2", 
+                       "2018-4-3"))
+  pub_hol_vec <- demand.data %>% 
+    filter(date(datetime) %in% pub_hol_vec) %>% 
+    pull(public_holiday)
+  expect_true(all(pub_hol_vec == 0))
+})
+
+test_that("demand loads correctly", {
+  demand.summ <- demand.data %>% 
+    group_by(date = date(datetime)) %>% 
+    summarise(demand_mw = sum(demand_mw, na.rm = T),
+              .groups = "drop")
+  
+  expect_gt(min(demand.summ$demand_mw), 2)
+  expect_lt(max(demand.summ$demand_mw), 80)
+})
