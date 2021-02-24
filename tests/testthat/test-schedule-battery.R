3b022f6ffc993f8a79fd31246d59807ef94293bc
Cameron Roach <camroach87@gmail.com>
2021-02-24 22:00:16 +1100

    Add battery schedule and file format tests

--- tests/testthat/test-schedule-battery.R
+++ tests/testthat/test-schedule-battery.R
@@ -1,10 +1,11 @@
+setwd("../..")
+
 library(podEnergyComp)
 library(tidyverse)
 library(lubridate)
 
-setwd("../..")
-
 fcst_start_date <- ymd("2018-10-16")
+# fcst_start_date <- ymd("2018-07-23")
 demand.data <- load_demand_data("inst/extdata/pod_ds_task1")
 demand.cv <- cv_ts_folds(demand.data$datetime,
                          start_date = fcst_start_date,
@@ -35,28 +36,61 @@ fcst_df <- full_join(pv.pred_df, demand.pred_df, by = "datetime") %>%
   mutate(period = 2*hour(datetime) + minute(datetime)/30 + 1)
 b_sched <- schedule_battery(fcst_df)
 bat_df <- format_charge_data(b_sched$B)
-
 B <- b_sched$B
 C <- b_sched$C
 L <- b_sched$L
 
 test_that("test battery schedule", {
-  expect_equivalent(C[,2:32], 0.5*t(apply(B, 1, cumsum))[,1:31])
-  expect_gte(min(B, na.rm=T), -2.5)
-  expect_lte(max(B, na.rm=T), 2.5)
-  expect_lte(max(apply(t(B[,1:31]), 2, cumsum)), 12)
-  expect_lte(max(C), 6)
-  expect_equal(min(C), 0)
-  expect_true(all(B[,32:42] <= 0))
-  expect_true(all(B[,1:31] >= 0))
+  # check energy stored matches charges/discharges
+  expect_equal(unname(C[,2:32]), 
+               unname(0.5*t(apply(B, 1, cumsum))[,1:31]))
+  expect_equal(dimnames(B), dimnames(C))
+  for (i in rownames(B)) {
+    # charge stored tests
+    expect_true(all(C[!!i,1:10] == 0))
+    expect_true(all(C[!!i,11:42] >= 0))
+    expect_true(all(C[!!i,43:48] == 0))
+    expect_equal(max(C[!!i,]), 6)
+    expect_equal(min(C[!!i,]), 0)
+    # charge/discharge tests
+    expect_equal(max(cumsum(B[!!i,1:31])), 12)
+    expect_equal(min(cumsum(B[!!i,32:42])), -12)
+    expect_gte(min(B[!!i,], na.rm=T), -2.5)
+    expect_lte(max(B[!!i,], na.rm=T), 2.5)
+    expect_true(all(B[!!i,1:10] == 0))
+    expect_true(all(B[!!i,11:31] >= 0))
+    expect_true(all(B[!!i,32:42] <= 0))
+    expect_true(all(B[!!i,43:48] == 0))
+  }
 })
 
 test_that("test battery data format", {
+  expect_equal(sum(is.na(bat_df)), 0)
+  expect_equal(colnames(bat_df), c("_id", "datetime", "charge_MW"))
+  expect_equal(dim(bat_df), c(336, 3))
+  expect_equal(bat_df$`_id`, 1:336)
+  
+  # check battery capacity (6 MWh) is not exceeded
   agg_bat_df <- bat_df %>% 
     group_by(date = date(datetime)) %>% 
-    mutate(cumsum_charge_MW = cumsum(charge_MW))
+    mutate(cumsum_charge_MW = cumsum(charge_MW)) %>% 
+    summarise(max_cumsum_charge_MW = max(cumsum_charge_MW),
+              .groups = "drop")
+  for (i in unique(agg_bat_df$date)) {
+    expect_equal(agg_bat_df %>% 
+                   filter(date == !!i) %>% 
+                   pull(max_cumsum_charge_MW), 
+                 12)
+  }
   
-  expect_equal(max(agg_bat_df$cumsum_charge_MW), 12)
+  # check for small values
+  expect_gt(min(abs(bat_df$charge_MW[bat_df$charge_MW != 0])),
+            1e-6)
+  
+  # make sure all date times differ by 30 minutes and in correct order
+  dts <- bat_df$datetime
+  expect_equal(as.numeric(dts[-1] - dts[-length(dts)]), 
+               rep(30, 335))
 })
 
 test_that("adjusted predicted profile is flat", {
