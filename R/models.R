c6644d6cb134eb1ca00d66847da000c748f31a03
Cameron Roach <camroach87@gmail.com>
2021-03-12 07:29:36 +1100

    Add new quantile forecasting model for PV

--- R/models.R
+++ R/models.R
@@ -13,7 +13,7 @@ pred_demand <- function(data,
                         ...) {
   
   response_idx <- which(colnames(data)=="demand_mw")
-  predict_lgbm(data, train_idx, test_idx, response_idx, ...)
+  predict_point(data, train_idx, test_idx, response_idx, ...)
 }
 
 #' Predict demand with models for each period
@@ -37,7 +37,7 @@ pred_demand_period <- function(data,
     period_train_idx <- intersect(period_idx, train_idx)
     period_test_idx <- intersect(period_idx, test_idx)
     pred_idx <- period_test_idx - min(test_idx) + 1
-    preds[pred_idx] <- predict_lgbm(
+    preds[pred_idx] <- predict_point(
       data[,-which(colnames(data)=="period")],  # remove as constant
       period_train_idx, 
       period_test_idx, 
@@ -63,12 +63,63 @@ pred_pv <- function(data,
                     test_idx,
                     ...) {
   response_idx <- which(colnames(data)=="pv_power_mw")
-  pv_pred <- predict_lgbm(data, train_idx, test_idx, response_idx, ...)
+  pv_pred <- predict_point(data, train_idx, test_idx, response_idx, ...)
   ifelse(pv_pred < 1e-2, 0, pv_pred)  # TODO: make 1e-2 a threshold argument 1e-2 MW is 100 W, which I think is reasonable to ignore
 }
 
 
-#' Predict using lightgbm
+#' Predict PV power with quantile selection
+#'
+#' @param data (data frame) Data frame containing predictors, response variable `pv_power_mw` and `datetime`.
+#' @param train_idx (integer) Vector of integers specifying which rows to use for training model
+#' @param test_idx (integer) Vector of integers specifying which rows to use for testing model
+#' @param alpha (numeric) Vector of quantiles 0.01-0.99.
+#' @param ... Additional arguments passed to `lgb.fit`.
+#'
+#' @return
+#' @export
+pred_pv_quantile <- function(data,
+                             train_idx,
+                             test_idx,
+                             alpha = seq(0.5,0.9,0.01),
+                             ...) {
+  dots = list(...)
+  datetimes <- select(data, datetime)
+  data <- select(data, -datetime)
+  response_idx <- which(colnames(data)=="pv_power_mw")
+  pred_list <- map(
+    set_names(alpha), 
+    ~ pred_quantile(data, train_idx, test_idx, response_idx, alpha = .x, dots)
+  )
+  
+  # Combine quantile predictions
+  pv_pred <- bind_cols(datetimes[test_idx,], as_tibble(pred_list)) %>% 
+    pivot_longer(cols = -datetime, names_to = "quantile", 
+                 values_to = "pv_power_mw") %>% 
+    mutate(quantile = as.numeric(quantile),
+           date = date(datetime))
+  
+  # Find quantile closest to 6 MWh for each day
+  # FIXME: hard coded MW and MWh values here
+  # FIXME: 0.5 quantile not chosen sometimes despite being > 6 MWh. Need to
+  # check if 0.5 quantile is already > 6 MWh and then start looking through
+  # higher quantiles.
+  pv_pred_sum <- pv_pred %>% 
+    group_by(date, quantile) %>% 
+    summarise(pv_power_mwh = sum(pv_power_mw)/2) %>%  # convert MW to MWh
+    group_by(date) %>% 
+    filter(abs(pv_power_mwh - 6) == min(abs(pv_power_mwh - 6))) %>% 
+    ungroup()
+  
+  pv_pred <- pv_pred %>% 
+    inner_join(pv_pred_sum, by = c("date", "quantile")) %>% 
+    pull(pv_power_mw)
+  
+  ifelse(pv_pred < 1e-2, 0, pv_pred)  # TODO: make 1e-2 a threshold argument 1e-2 MW is 100 W, which I think is reasonable to ignore
+}
+
+
+#' Predict point prediction using lightgbm
 #'
 #' @param data 
 #' @param train_idx 
@@ -76,10 +127,8 @@ pred_pv <- function(data,
 #' @param response_idx 
 #' @param ... 
 #'
-#' @return
-#'
 #' @importFrom lightgbm lgb.train lgb.Dataset
-predict_lgbm <- function(data, train_idx, test_idx, response_idx, ...) {
+predict_point <- function(data, train_idx, test_idx, response_idx, ...) {
   data.train <- as.matrix(data[train_idx,])
   data.train_label <- data.train[, response_idx, drop = TRUE]
   data.train <- data.train[, -response_idx, drop = FALSE]
@@ -95,6 +144,45 @@ predict_lgbm <- function(data, train_idx, test_idx, response_idx, ...) {
     ),
     verbose = 0,
     force_col_wise = TRUE,
+    ...
+  )
+  
+  predict(lgb.fit, data.test)
+}
+
+
+#' Predict quantile predictions using lightgbm
+#'
+#' @param data 
+#' @param train_idx 
+#' @param test_idx 
+#' @param response_idx 
+#' @param alpha
+#' @param ... 
+#'
+#' @importFrom lightgbm lgb.train lgb.Dataset
+pred_quantile <- function(data, train_idx, test_idx, response_idx, alpha, ...) {
+  message(paste("Fitting quantile", alpha, "..."))
+  
+  # FIXME: this is a duplication of the above code. Convert to function.
+  data.train <- as.matrix(data[train_idx,])
+  data.train_label <- data.train[, response_idx, drop = TRUE]
+  data.train <- data.train[, -response_idx, drop = FALSE]
+  
+  data.test <- as.matrix(data[test_idx,])
+  data.test_label <- data.test[, response_idx, drop = TRUE]
+  data.test <- data.test[, -response_idx, drop = FALSE]
+  
+  lgb.fit <- lgb.train(
+    data = lgb.Dataset(
+      data = data.train,
+      label = data.train_label
+    ),
+    verbose = 0,
+    force_col_wise = TRUE,
+    obj = "quantile",
+    eval = "quantile",
+    alpha = alpha,
     ...
   )
   
